2e85aafef1e36422b4b133509f0364521ce400a9
Sominhwan <thalsghks@naver.com>
2024-03-15 00:29:08 +0900

    20240315 SMH [공통위젯] 광고 위젯 수정

--- lib/widget/advertisement_widget.dart
+++ lib/widget/advertisement_widget.dart
@@ -10,7 +10,7 @@ class AdvertisementWidget extends StatefulWidget {
 }
 
 class _AdvertisementWidgetState extends State<AdvertisementWidget> {
-  bool saveSignatureFlag = false;
+  bool checkboxFlag = false;
 
   @override
   Widget build(BuildContext context) {
@@ -31,11 +31,42 @@ class _AdvertisementWidgetState extends State<AdvertisementWidget> {
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                const SizedBox(width: 15),
-                const Text(
-                  '일주일동안 보지않기',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 13, color: Colors.black, fontWeight: FontWeight.w500),
+                const SizedBox(width: 5),
+                InkWell(
+                  child: Row(
+                    children: [
+                      Transform.scale(
+                        scale: 0.8, // 원하는 배율로 조정
+                        child: Checkbox(
+                          value: checkboxFlag,
+                          onChanged: (value) {
+                            setState(() {
+                              checkboxFlag = value!;
+                            });
+                          },
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(0),
+                          ),
+                          side: MaterialStateBorderSide.resolveWith((states) {
+                            return const BorderSide(color: Colors.grey, width: 1);
+                          }),
+                          visualDensity: VisualDensity.compact,
+                          checkColor: Colors.grey, // 체크 마크 색상
+                          activeColor: Colors.white, // 박스 배경 색상
+                        ),
+                      ),
+                      const Text(
+                        '일주일동안 보지않기',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 13, color: Colors.black, fontWeight: FontWeight.w500),
+                      ),
+                    ],
+                  ),
+                  onTap: () {
+                    setState(() {
+                      checkboxFlag = !checkboxFlag;
+                    });
+                  },
                 ),
                 const Spacer(),
                 InkWell(
@@ -48,6 +79,7 @@ class _AdvertisementWidgetState extends State<AdvertisementWidget> {
                     Navigator.of(context).pop(null);
                   },
                 ),
+
                 const SizedBox(width: 15),
               ],
             ),
