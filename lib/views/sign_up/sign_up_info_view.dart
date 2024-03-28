a4d786c61feb21510dd5bd84ea4d50083739607f
Sominhwan <thalsghks@naver.com>
2024-03-29 00:54:10 +0900

    20240329SMH [공통위젯] 회원가입 폼 수정

--- lib/views/sign_up/sign_up_info_view.dart
+++ lib/views/sign_up/sign_up_info_view.dart
@@ -1,5 +1,7 @@
 
 
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 class SignUpInfoView extends StatefulWidget {
@@ -18,28 +20,73 @@ class _SignUpInfoViewState extends State<SignUpInfoView> {
   final TextEditingController _idController = TextEditingController();
   final TextEditingController _pwController = TextEditingController();
   final TextEditingController _rePwController = TextEditingController();
+  final TextEditingController _phoneNumController = TextEditingController();
+
+  // FocusNode 인스턴스 생성
+  final FocusNode _nameFocus = FocusNode();
+  final FocusNode _emailFocus = FocusNode();
+  final FocusNode _idFocus = FocusNode();
+  final FocusNode _passwordFocus = FocusNode();
+  final FocusNode _rePasswordFocus = FocusNode();
+  final FocusNode _phoneNumFocus = FocusNode();
+
   late bool _isDuplicateCheckRequired = false; // 중복확인 validator 상태 관리 변수 추가
   late bool passwordFlag = false;
   late bool passwordFlag2 = false;
   bool _obscureText = true;
   bool _obscureText2 = true;
+  bool _isNextButtonEnabled = false;
+
+  void _fieldFocusChange(BuildContext context, FocusNode currentFocus, FocusNode nextFocus) {
+    currentFocus.unfocus();
+    FocusScope.of(context).requestFocus(nextFocus);
+  }
+
+  void _updateNextButtonState() {
+    final allFieldsFilled = _nameController.text.isNotEmpty &&
+        _emailController.text.isNotEmpty &&
+        _idController.text.isNotEmpty &&
+        _pwController.text.isNotEmpty &&
+        _rePwController.text.isNotEmpty &&
+        _phoneNumController.text.isNotEmpty;
+
+    // 상태 업데이트
+    setState(() {
+      _isNextButtonEnabled = allFieldsFilled;
+    });
+  }
+
   @override
   void initState() {
     super.initState();
     // 리스너 추가: 사용자가 텍스트 필드에 입력할 때마다 UI 업데이트
     _nameController.addListener(() {
+      _updateNextButtonState();
       setState(() {});
     });
     _emailController.addListener(() {
+      _updateNextButtonState();
       setState(() {});
     });
     _idController.addListener(() {
-      setState(() {});
+      _updateNextButtonState();
+      // If the user starts editing the ID again, indicate that we need to re-check for duplicates.
+      if (!_isDuplicateCheckRequired && _idController.text.isNotEmpty) {
+        setState(() {
+          _isDuplicateCheckRequired = true;
+        });
+      }
     });
     _pwController.addListener(() {
+      _updateNextButtonState();
       setState(() {});
     });
     _rePwController.addListener(() {
+      _updateNextButtonState();
+      setState(() {});
+    });
+    _phoneNumController.addListener(() {
+      _updateNextButtonState();
       setState(() {});
     });
   }
@@ -47,8 +94,19 @@ class _SignUpInfoViewState extends State<SignUpInfoView> {
   @override
   void dispose() {
     // 위젯이 dispose될 때 리스너 제거 및 컨트롤러 해제
-    _nameController.dispose();
-    _pwController.dispose();
+    // _nameController.dispose();
+    // _pwController.dispose();
+    // _idController.dispose();
+    // _pwController.dispose();
+    // _rePwController.dispose();
+    // _phoneNumController.dispose();
+
+    _nameFocus.dispose();
+    _emailFocus.dispose();
+    _idFocus.dispose();
+    _passwordFocus.dispose();
+    _rePasswordFocus.dispose();
+    _phoneNumFocus.dispose();
     super.dispose();
   }
 
@@ -88,6 +146,10 @@ class _SignUpInfoViewState extends State<SignUpInfoView> {
                         TextFormField(
                           controller: _nameController,
                           cursorColor: Colors.black,
+                          focusNode: _nameFocus,
+                          onFieldSubmitted: (term) {
+                            _fieldFocusChange(context, _nameFocus, _emailFocus); // 다음 필드로 포커스 이동
+                          },
                           decoration: InputDecoration(
                             border: const OutlineInputBorder(borderSide: BorderSide(color: Colors.transparent), borderRadius: BorderRadius.zero,), // 기본 테두리 색상
                             enabledBorder: const OutlineInputBorder(borderSide: BorderSide(color: Color.fromRGBO(203, 203, 203, 1)), borderRadius: BorderRadius.zero,), // 비활성화 상태의 테두리 색상
@@ -137,6 +199,10 @@ class _SignUpInfoViewState extends State<SignUpInfoView> {
                         TextFormField(
                           controller: _emailController,
                           cursorColor: Colors.black,
+                          focusNode: _emailFocus,
+                          onFieldSubmitted: (term) {
+                            _fieldFocusChange(context, _emailFocus, _idFocus); // 다음 필드로 포커스 이동
+                          },
                           decoration: InputDecoration(
                             border: const OutlineInputBorder(borderSide: BorderSide(color: Colors.transparent), borderRadius: BorderRadius.zero,), // 기본 테두리 색상
                             enabledBorder: const OutlineInputBorder(borderSide: BorderSide(color: Color.fromRGBO(203, 203, 203, 1)), borderRadius: BorderRadius.zero,), // 비활성화 상태의 테두리 색상
@@ -190,6 +256,10 @@ class _SignUpInfoViewState extends State<SignUpInfoView> {
                                 child: TextFormField(
                                   controller: _idController,
                                   cursorColor: Colors.black,
+                                  focusNode: _idFocus,
+                                  onFieldSubmitted: (term) {
+                                    _fieldFocusChange(context, _idFocus, _passwordFocus); // 다음 필드로 포커스 이동
+                                  },
                                   decoration: InputDecoration(
                                     border: const OutlineInputBorder(borderSide: BorderSide(color: Colors.transparent), borderRadius: BorderRadius.zero,), // 기본 테두리 색상
                                     enabledBorder: const OutlineInputBorder(borderSide: BorderSide(color: Color.fromRGBO(203, 203, 203, 1)), borderRadius: BorderRadius.zero,), // 비활성화 상태의 테두리 색상
@@ -221,10 +291,14 @@ class _SignUpInfoViewState extends State<SignUpInfoView> {
 
                                   },
                                   validator: (value) {
-                                    if (_isDuplicateCheckRequired) {
-                                      return '중복 확인이 필요합니다.'; // 조건에 따른 메시지 변경
+                                    if (value == null || value.isEmpty) {
+                                      return '필수 입력 항목입니다.';
+                                    } else if (_isDuplicateCheckRequired) {
+                                      // 중복 확인이 필요한 상태에서 중복 확인 버튼을 클릭하지 않았을 경우 메시지 출력
+                                      log('중복확인');
+                                      return '아이디 중복 확인이 필요합니다.';
                                     }
-                                    return value == '' ? '필수 입력 항목입니다.' : null;
+                                    return null; // 위의 조건에 해당하지 않는 경우, 유효한 입력으로 간주
                                   },
                                 ),
                               ),
@@ -232,10 +306,10 @@ class _SignUpInfoViewState extends State<SignUpInfoView> {
                               Column(
                                 children: [
                                   SizedBox(
-                                    height: 50,
+                                    height: 48,
                                     child: InkWell(
                                       child: Container(
-                                        width: 95,
+                                        width: 90,
                                         decoration: BoxDecoration(
                                           border: Border.all(
                                             color: const Color.fromRGBO(203, 203, 203, 1), // Set border color
@@ -248,8 +322,9 @@ class _SignUpInfoViewState extends State<SignUpInfoView> {
                                       ),
                                       onTap: () {
                                         setState(() {
-                                          _isDuplicateCheckRequired = true; // 상태 업데이트
+                                          _isDuplicateCheckRequired = false; // 상태 업데이트
                                           formKey.currentState!.validate();
+                                          // formKey.currentState!.validate();
                                         });
                                       },
                                     ),
@@ -260,7 +335,6 @@ class _SignUpInfoViewState extends State<SignUpInfoView> {
                           ),
                         ),
                         /// 비밀번호
-                        const SizedBox(height: 20),
                         const Row(
                           crossAxisAlignment: CrossAxisAlignment.baseline,
                           textBaseline: TextBaseline.alphabetic, // Align based on text baseline
@@ -284,6 +358,10 @@ class _SignUpInfoViewState extends State<SignUpInfoView> {
                           },
                           validator: (value) => value == '' ? '필수 입력 항목입니다.' : null,
                           obscureText: _obscureText,
+                          focusNode: _passwordFocus,
+                          onFieldSubmitted: (term) {
+                            _fieldFocusChange(context, _passwordFocus, _rePasswordFocus); // 다음 필드로 포커스 이동
+                          },
                           decoration: InputDecoration(
                             border: const OutlineInputBorder(borderSide: BorderSide(color: Colors.transparent), borderRadius: BorderRadius.zero,), // 기본 테두리 색상
                             enabledBorder: const OutlineInputBorder(borderSide: BorderSide(color: Color.fromRGBO(203, 203, 203, 1)), borderRadius: BorderRadius.zero,), // 비활성화 상태의 테두리 색상
@@ -368,6 +446,10 @@ class _SignUpInfoViewState extends State<SignUpInfoView> {
                           },
                           validator: (value) => value == '' ? '필수 입력 항목입니다.' : null,
                           obscureText: _obscureText2,
+                          focusNode: _rePasswordFocus,
+                          onFieldSubmitted: (term) {
+                            _fieldFocusChange(context, _rePasswordFocus, _phoneNumFocus); // 다음 필드로 포커스 이동
+                          },
                           decoration: InputDecoration(
                             border: const OutlineInputBorder(borderSide: BorderSide(color: Colors.transparent), borderRadius: BorderRadius.zero,), // 기본 테두리 색상
                             enabledBorder: const OutlineInputBorder(borderSide: BorderSide(color: Color.fromRGBO(203, 203, 203, 1)), borderRadius: BorderRadius.zero,), // 비활성화 상태의 테두리 색상
@@ -441,6 +523,54 @@ class _SignUpInfoViewState extends State<SignUpInfoView> {
                           ),
                         ),
                         const SizedBox(height: 20),
+                        const Row(
+                          crossAxisAlignment: CrossAxisAlignment.baseline,
+                          textBaseline: TextBaseline.alphabetic, // Align based on text baseline
+                          children: [
+                            Text('휴대폰 번호', style: TextStyle(color: Colors.black, fontSize: 14)),
+                            Baseline(
+                              baselineType: TextBaseline.alphabetic,
+                              baseline: BorderSide.strokeAlignCenter,
+                              child: Text(' *', style: TextStyle(fontSize: 14, color: Colors.red)),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 5),
+                        TextFormField(
+                          controller: _phoneNumController,
+                          cursorColor: Colors.black,
+                          focusNode: _phoneNumFocus,
+                          decoration: InputDecoration(
+                            border: const OutlineInputBorder(borderSide: BorderSide(color: Colors.transparent), borderRadius: BorderRadius.zero,), // 기본 테두리 색상
+                            enabledBorder: const OutlineInputBorder(borderSide: BorderSide(color: Color.fromRGBO(203, 203, 203, 1)), borderRadius: BorderRadius.zero,), // 비활성화 상태의 테두리 색상
+                            focusedBorder: const OutlineInputBorder(borderSide: BorderSide(color: Colors.blue), borderRadius: BorderRadius.zero), // 활성화 상태의 테두리 색상
+                            filled: true,
+                            fillColor: Colors.white,
+                            hintText: '휴대폰 번호 입력',
+                            hintStyle: const TextStyle(color: Colors.grey, fontSize: 16),
+                            contentPadding: const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                            errorStyle: const TextStyle(color: Colors.redAccent),
+                            errorBorder: const OutlineInputBorder(borderSide: BorderSide(color: Colors.redAccent), borderRadius: BorderRadius.zero),
+                            focusedErrorBorder: const OutlineInputBorder(borderSide: BorderSide(color: Colors.redAccent), borderRadius: BorderRadius.zero),
+                            suffixIcon: _phoneNumController.text.isNotEmpty ?
+                            InkWell(
+                              splashColor: Colors.transparent,
+                              highlightColor: Colors.transparent,
+                              child: const Icon(Icons.cancel, color: Colors.grey,),
+                              onTap: () {
+                                _phoneNumController.clear();
+                                setState(() {});
+                              },
+                            ) : null,
+                          ),
+                          enabled: true,
+                          style: const TextStyle(color: Colors.black), // 텍스트 색상 변경
+                          maxLines: 1,
+                          onChanged: (value) {
+
+                          },
+                          validator: (value) => value == '' ? '필수 입력 항목입니다.' : null,
+                        ),
                       ],
                     ),
                   ),
@@ -448,23 +578,45 @@ class _SignUpInfoViewState extends State<SignUpInfoView> {
               ),
             ),
           ),
-        ],
-      ),
-      bottomNavigationBar: InkWell(
-        onTap: () {
-          if(formKey.currentState!.validate()) {
-            formKey.currentState!.save();
-            // _fileController?.save();
-          }
-        },
-        child: Container(
-          width: double.infinity,
-          height: 50,
-          color: const Color.fromRGBO(220, 220, 220, 1),
-          child: const Center(
-              child: Text('다음', style: TextStyle(color: Colors.white, fontSize: 18))
+          _isNextButtonEnabled ?
+          InkWell(
+              onTap: () {
+                // Validate the form first.
+                bool isValid = formKey.currentState!.validate();
+
+                if (isValid && !_isDuplicateCheckRequired) {
+                  // Form is valid, and duplicate check has passed, proceed to save form or move to next step.
+                  formKey.currentState!.save();
+                  log('이상 없음');
+                  // Proceed with the next actions...
+                } else {
+                  // Optionally, handle the case where the form is valid but the duplicate check hasn't been performed.
+                  if (!_isDuplicateCheckRequired) {
+
+                  }
+                }
+              },
+              child: Container(
+                width: double.infinity,
+                height: 50,
+                color: Colors.blueAccent,
+                child: const Center(
+                    child: Text('다음', style: TextStyle(color: Colors.white, fontSize: 18))
+                ),
+              )
+          ) :
+          InkWell(
+              onTap: null,
+              child: Container(
+                width: double.infinity,
+                height: 50,
+                color: const Color.fromRGBO(220, 220, 220, 1),
+                child: const Center(
+                    child: Text('다음', style: TextStyle(color: Colors.white, fontSize: 18))
+                ),
+              )
           ),
-        )
+        ],
       ),
     );
   }
