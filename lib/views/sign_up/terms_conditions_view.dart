88f8d601d3db85bd1823f00638bd92336cb6891c
Sominhwan <thalsghks@naver.com>
2024-04-14 22:33:11 +0900

    20240414 SMH [공통위젯] 회원가입 이용약관 공통 페이지

---
+++ lib/views/sign_up/terms_conditions_view.dart
@@ -0,0 +1,42 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TermsConditionsView extends StatefulWidget {
+  final String? title;
+  const TermsConditionsView({super.key, this.title});
+  static String get className => 'TermsConditionsView';
+  static String get path => '/termsConditions';
+
+  @override
+  State<TermsConditionsView> createState() => _TermsConditionsViewState();
+}
+
+class _TermsConditionsViewState extends State<TermsConditionsView> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title ?? ''),
+        surfaceTintColor: Colors.white,
+        leading: IconButton(
+          icon: const Icon(Icons.close),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+      body: const Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
