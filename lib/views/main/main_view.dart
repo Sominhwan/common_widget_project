c99d2a8ec188a47d17bc0d017adc6679da4d3a10
Sominhwan <thalsghks@naver.com>
2024-04-22 23:44:22 +0900

    20240422 SMH [공통위젯] 검색 위젯 수정

--- lib/views/main/main_view.dart
+++ lib/views/main/main_view.dart
@@ -181,9 +181,9 @@ class _MainViewState extends State<MainView> {
                 context: context,
                 builder: (BuildContext context) {
                   return CustomDateSelectorWidget(
-                    selectedDt: DateTime.now(), // Current date as placeholder
-                    minDt: DateTime(2020), // Minimum date as placeholder
-                    maxDt: DateTime(2025), // Maximum date as placeholder
+                    selectedDt: DateTime.now(),
+                    minDt: DateTime(2020),
+                    maxDt: DateTime(2025),
                     onChanged: (DateTime newDate) {
                       print(newDate);
                     },
