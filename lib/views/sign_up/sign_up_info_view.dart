2307a61b64903f8e80afb590b0771e8c25e62ca2
Sominhwan <thalsghks@naver.com>
2024-05-07 23:06:20 +0900

    20240507 SMH [공통위젯] 검색 위젯 수정

--- lib/views/sign_up/sign_up_info_view.dart
+++ lib/views/sign_up/sign_up_info_view.dart
@@ -493,7 +493,7 @@ class _SignUpInfoViewState extends State<SignUpInfoView> {
                                                 .transparent, // 클릭 영역을 명확히 하기 위해 투명한 배경 색상 추가
                                             padding: EdgeInsets.zero, // 여기서는 패딩을 제거
                                             child: const Icon(
-                                                Icons.clear), // Icon 크기를 직접 지정
+                                                Icons.clear)
                                           ),
                                         ),
                                       ],
